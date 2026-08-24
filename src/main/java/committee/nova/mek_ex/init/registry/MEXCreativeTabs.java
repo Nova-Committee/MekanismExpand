70f4b99694c1f146b5fb51935448ce0c3cd421e6
cu6 <3077476144@qq.com>
2026-08-24 23:44:52 +0800

    修改创造模式物品栏与配方

--- src/main/java/committee/nova/mek_ex/init/registry/MEXCreativeTabs.java
+++ src/main/java/committee/nova/mek_ex/init/registry/MEXCreativeTabs.java
@@ -4,33 +4,32 @@ import committee.nova.mek_ex.MekEXMod;
 import committee.nova.mek_ex.init.enums.MEXLang;
 import mekanism.common.registration.MekanismDeferredHolder;
 import mekanism.common.registration.impl.CreativeTabDeferredRegister;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
 public final class MEXCreativeTabs {
 
-    public static final CreativeTabDeferredRegister CREATIVE_TABS = new CreativeTabDeferredRegister(MekEXMod.MOD_ID, MEXCreativeTabs::addToExistingTabs);
-    public static final MekanismDeferredHolder<CreativeModeTab, CreativeModeTab> MEKANISM_EXPAND = CREATIVE_TABS.registerMain(
-          MEXLang.MEKANISM_EXPAND, MEXBlocks.basic_wind_generator.getItemHolder(), builder -> builder
-                .withTabsBefore(CreativeModeTabs.FUNCTIONAL_BLOCKS)
-                .displayItems((parameters, output) -> {
-                    output.accept(MEXItems.capacity_upgrade);
-                    CreativeTabDeferredRegister.addToDisplay(MEXBlocks.BLOCKS, output);
-                }));
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MekEXMod.MOD_ID);
 
-    private MEXCreativeTabs() {
-    }
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> CREATIVE_TAB = CREATIVE_TABS.register("expand_group", () -> CreativeModeTab.builder()
+            .title(Component.translatable(MEXLang.MEKANISM_EXPAND.getTranslationKey()))
+            .icon(MEXItems.capacity_upgrade.get()::getDefaultInstance)
+            .displayItems((parameters, output) -> {
+                output.accept(MEXItems.capacity_upgrade);
+                output.accept(MEXBlocks.basic_wind_generator);
+                output.accept(MEXBlocks.advanced_wind_generator);
+                output.accept(MEXBlocks.elite_wind_generator);
+                output.accept(MEXBlocks.ultimate_wind_generator);
+                output.accept(MEXBlocks.nuclear_control_tank);
+                output.accept(MEXBlocks.nuclear_control_valve);
+            })
+        .build());
 
-    private static void addToExistingTabs(BuildCreativeModeTabContentsEvent event) {
-        ResourceKey<CreativeModeTab> tabKey = event.getTabKey();
-        if (tabKey == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
-            CreativeTabDeferredRegister.addToDisplay(event, MEXBlocks.basic_wind_generator, MEXBlocks.advanced_wind_generator,
-                  MEXBlocks.elite_wind_generator, MEXBlocks.ultimate_wind_generator,
-                  MEXBlocks.nuclear_control_tank, MEXBlocks.nuclear_control_valve);
-        } else if (tabKey == CreativeModeTabs.INGREDIENTS) {
-            CreativeTabDeferredRegister.addToDisplay(event, MEXItems.capacity_upgrade);
-        }
-    }
 }
